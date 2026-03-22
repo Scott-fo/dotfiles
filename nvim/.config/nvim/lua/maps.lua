42748f5a9160cd984c57cd9b4e1a2bdaa82d4132
scott <scottfo@live.com>
2026-03-22 00:34:29 +0000

    add lazy

--- nvim/.config/nvim/lua/maps.lua
+++ nvim/.config/nvim/lua/maps.lua
@@ -1,4 +1,54 @@
 local keymap = vim.keymap
+local api = vim.api
+
+local function reload_changed_buffers()
+  for _, buf in ipairs(api.nvim_list_bufs()) do
+    if api.nvim_buf_is_loaded(buf) and vim.bo[buf].buftype == "" then
+      api.nvim_buf_call(buf, function()
+        vim.cmd("silent! checktime")
+      end)
+    end
+  end
+end
+
+local function open_lazygit()
+  vim.cmd("silent! wall")
+
+  local buf = api.nvim_create_buf(false, true)
+  local width = math.floor(vim.o.columns * 0.9)
+  local height = math.floor(vim.o.lines * 0.9)
+  local row = math.floor((vim.o.lines - height) / 2)
+  local col = math.floor((vim.o.columns - width) / 2)
+
+  local win = api.nvim_open_win(buf, true, {
+    relative = "editor",
+    width = width,
+    height = height,
+    row = row,
+    col = col,
+    style = "minimal",
+    border = "rounded",
+  })
+
+  vim.bo[buf].bufhidden = "wipe"
+  vim.bo[buf].filetype = "lazygit"
+  vim.wo[win].number = false
+  vim.wo[win].relativenumber = false
+  vim.wo[win].signcolumn = "no"
+
+  vim.fn.termopen("lazygit", {
+    on_exit = function()
+      vim.schedule(function()
+        if api.nvim_win_is_valid(win) then
+          api.nvim_win_close(win, true)
+        end
+        reload_changed_buffers()
+      end)
+    end,
+  })
+
+  vim.cmd("startinsert")
+end
 
 -- Don't yank on x
 keymap.set('n', 'x', '"_x')
@@ -33,5 +83,7 @@ keymap.set('n', '<C-w><down>', '<C-w>-')
 
 keymap.set('n', '<C-d>', '<C-d>zz')
 keymap.set('n', '<C-u>', '<C-u>zz')
+
+keymap.set({ 'n', 'i' }, '<M-g>', open_lazygit, { desc = 'Open lazygit' })
 
 vim.api.nvim_set_keymap('i', 'jj', '<Esc>', { noremap = true })
